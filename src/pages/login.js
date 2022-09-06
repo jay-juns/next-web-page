54e8d88515253fc2a7e26d313a89caaeeeb9e9bf
jay-juns <necrozerg2@naver.com>
2022-09-06 23:17:05 +0900

    add login signup

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Signup = () => {
+    return (
+        <div>
+            <Head>
+                <title>next web page - Sign up</title>
+            </Head>
+            Its Sign up page
+        </div>
+    )
+};
+
+export default Signup;
---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Login = () => {
+    return (
+        <div>
+            <Head>
+                <title>next web page - Log in</title>
+            </Head>
+            Its Log in page
+        </div>
+    )
+};
+
+export default Login;
