9394edb0469ac8e4902b9080ccfd8140844cd222
jay-juns <necrozerg2@naver.com>
2022-09-12 14:55:58 +0900

    재수정

--- src/pages/introduction.js
+++ src/pages/introduction.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 
 const Instroduction = () => {
     return (
-        <div>
-            <Head>
-                <title>Visecare - Why Visecare</title>
-            </Head>
-            <div>
-                Instroduction
-            </div>
-        </div>
+      <div>
+          <Head>
+              <title>Visecare - Why Visecare</title>
+          </Head>
+          <div>
+              Instroduction
+          </div>
+      </div>
     )
 };
 
